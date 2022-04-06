697ed3aaf0d96939464c9bcca393ffdbd3e364e2
ShowkatAliRakib <rakibmd54496313@gmail.com>
2022-04-06 19:35:18 +0600

    chart-added

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,11 +1,90 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Area, AreaChart, CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashboard = () => {
+    const [chart, setchart]=useState([])
+
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res=>res.json())
+        .then(data=> setchart(data))
+        
+    },[])
+    // console.log( chart);
     return (
-        <div>
-            <p>Dashboard</p>
-        </div>
+
+
+    //     <ResponsiveContainer width="100%" height="100%">
+    //     <LineChart width={300} height={100} data={chart}>
+    //       <Line type="monotone" dataKey="sell" stroke="#8884d8" strokeWidth={2} />
+    //     </LineChart>
+    //   </ResponsiveContainer>
+<div>
+    
+<div>
+        <ResponsiveContainer width="100%" height={400}>
+                    <LineChart
+                        width={800}
+                        height={400}
+                        data={chart}
+                        syncId="anyId"
+                        margin={{
+                            top: 10,
+                            right: 30,
+                            left: 0,
+                            bottom: 0,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <Tooltip />
+                        <XAxis dataKey="month" />
+                        <YAxis />
+                        <Tooltip />
+                        <Line type="monotone" dataKey="sell" stroke="#82ca9d" fill="#82ca9d" />
+                    </LineChart>
+                </ResponsiveContainer>
+    </div>
+
+
+    <div>
+    <AreaChart
+      width={800}
+      height={400}
+      data={chart}
+      margin={{
+        top: 10,
+        right: 30,
+        left: 0,
+        bottom: 0
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Area type="monotone" dataKey="investment" stroke="#8884d8" fill="#8884d8" />
+    </AreaChart>
+    </div>
+</div>
+
+
+
+                    
+
+    //   <LineChart width={800} height={500} data={data}>
+    //       <Line datakey="sell"></Line>
+    //       <Line datakey='investment'  ></Line>
+         
+    //       {/* <Line  datakey={'month'}></Line> */}
+    //       <XAxis datakey="month"/>
+    //       <Tooltip/>
+    //       <YAxis datakey='revenue'></YAxis>
+    //   </LineChart>
+    // <div>
+        
+    // </div>
     );
-};
+   
+};   
 
 export default Dashboard;
